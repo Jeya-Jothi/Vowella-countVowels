f7c4de6cb5a9f50eba0a3c8aab329465eb66fbb6
Jeya Jothi <jeyajothi108@gmail.com>
2025-07-01 10:39:53 +0530

    Complete logic

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,18 +1,38 @@
+import { useState } from "react";
 import "./Card.css";
 function Card() {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(0);
+
+  const handleChange = (e) => {
+    const newText = e.target.value;
+    setText(newText);
+  };
+
+  const countVowels = () => {
+    const textArray = text.split("");
+    console.log(textArray);
+    const newText = textArray.filter((t) => {
+      return vowels.includes(t);
+    });
+    console.log(newText);
+
+    setCount(newText.length);
+  };
   return (
     <div className="card">
       <div className="card-head">
         <p>Count Vowels</p>
       </div>
       <div className="card-input">
-        <textarea placeholder="Enter your text here" />
+        <textarea placeholder="Enter your text here" onChange={handleChange} />
       </div>
       <div className="card-btn">
-        <button>Count</button>
+        <button onClick={countVowels}>Count</button>
       </div>
       <div className="total-div">
-        <p>Total vowels</p>
+        <p>Total vowels: {count}</p>
       </div>
     </div>
   );
